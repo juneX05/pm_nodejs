b4a7b73c366d9cfa08d44bf7b12a2e1650b2d7ab
juneX05 <joelvankibona@gmail.com>
2020-12-31 13:06:21 +0300

    just some stuffs

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+app.listen(3000);
+
+
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', {root: __dirname});
+});
+
+app.get('/add-item', (req, res) => {
+    res.sendFile('./views/add-item.html', {root: __dirname});
+});
+
